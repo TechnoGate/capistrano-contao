8ff40eab583435988853d866ed18fcc6d0dfd5ee
Wael M. Nasreddine <wael.nasreddine@gmail.com>
2012-07-01 18:48:34 +0200

    Cleanup and add setup_if_needed task which call deploy:setup only if it hasnt been called before

--- lib/capistrano/ext/deploy.rb
+++ lib/capistrano/ext/deploy.rb
@@ -4,16 +4,16 @@ end
 
 Capistrano::Configuration.instance(:must_exist).load do
   namespace :deploy do
-    desc "Check if the remote is ready, should we run cap deploy:setup?"
+    desc 'Check if the remote is ready, should we run cap deploy:setup?'
     task :check_if_remote_ready, :roles => :app do
-      unless remote_file_exists?("#{shared_path}")
-        logger.important "ERROR: The project is not ready for deployment."
-        logger.important "please run `cap deploy:setup"
+      unless remote_file_exists?(shared_path)
+        logger.important 'ERROR: The project is not ready for deployment.'
+        logger.important 'please run `cap deploy:setup'
         abort
       end
     end
 
-    desc "Fix permissions"
+    desc 'Fix permissions'
     task :fix_permissions, :roles => :app do
       if exists?(:app_owner) or exists?(:app_group)
         run <<-CMD
@@ -27,27 +27,25 @@ Capistrano::Configuration.instance(:must_exist).load do
       run "chmod -R g+w #{fetch :latest_release}" if fetch(:group_writable, true)
     end
 
-    desc "[internal] create the required folders."
+    desc '[internal] create the required folders.'
     task :folders, :roles => :app do
       backup_path = fetch :backup_path, "#{fetch :deploy_to}/backups"
 
       run <<-CMD
-        #{try_sudo} mkdir -p #{fetch :deploy_to} &&
-        #{try_sudo} mkdir -p #{backup_path} &&
-        #{try_sudo} mkdir -p #{fetch :shared_path}/items &&
-        #{try_sudo} mkdir -p #{fetch :shared_path}/__system__ &&
-        #{try_sudo} mkdir -p #{fetch :shared_path}/config &&
-        #{try_sudo} mkdir -p #{fetch :shared_path}/items
+        #{try_sudo} mkdir -p \
+          #{fetch :deploy_to} \
+          #{backup_path} \
+          #{fetch :shared_path}/{items,__system__,config} \
+          #{fetch :logs_path, ''}
       CMD
-
-      if exists? :logs_path
-        run <<-CMD
-          #{try_sudo} mkdir -p #{fetch :logs_path}
-        CMD
-      end
     end
 
-    desc "[internal] Symlink public folder"
+    desc '[internal] Setup if needed'
+    task :setup_if_needed, :roles => :app do
+      setup unless main_task == 'deploy:setup'
+    end
+
+    desc '[internal] Symlink public folder'
     task :symlink_public_folders, :roles => :web, :except => { :no_release => true } do
       deploy_to = fetch :deploy_to
 
@@ -60,7 +58,7 @@ Capistrano::Configuration.instance(:must_exist).load do
               #{try_sudo} ln -nsf #{fetch :public_path} #{deploy_to}/#{folder}
             CMD
           rescue Capistrano::CommandError
-            logger.info "WARNING: I couldn't replace the old htdocs please do so manually"
+            logger.info "WARNING: I could not replace the #{folder}  please do so manually"
           end
           logger.info "The #{folder} folder has been moved to the #{deploy_to}/old/#{folder}"
         end
@@ -69,8 +67,8 @@ Capistrano::Configuration.instance(:must_exist).load do
   end
 
   # Dependencies
-  before "deploy", "deploy:check_if_remote_ready"
-  after "deploy:restart", "deploy:fix_permissions"
-  before "deploy:setup", "deploy:folders"
-  after "deploy:setup", "deploy:symlink_public_folders"
+  before 'deploy',         'deploy:check_if_remote_ready'
+  after  'deploy:restart', 'deploy:fix_permissions'
+  before 'deploy:setup',   'deploy:folders'
+  after  'deploy:setup',   'deploy:symlink_public_folders'
 end
